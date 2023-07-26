99d26f58311c6c5bbfcd84dbd719d1772f25868f
eyasu <daa282386@gmail.com>
2023-07-26 13:55:08 +0300

    0x01-caching_lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -15,14 +15,14 @@ class LRUCache(BaseCaching):
 
     def put(self, key, item):
         """updating an item from the cache."""
-        if key is None or item is None:
-            return
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", lru_key)
-            self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=True)
+        if not (key is None or item is None):
+            if key not in self.cache_data:
+                if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                    lru_key, _ = self.cache_data.popitem(True)
+                    print("DISCARD:", lru_key)
+                self.cache_data[key] = item
+                self.cache_data.move_to_end(key, last=False)
+        return
 
     def get(self, key):
         """Retrieve an item by key."""
