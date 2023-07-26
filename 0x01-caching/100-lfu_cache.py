566d14b8ddbdde25d0a53fd51a990853e5ae10ff
eyasu <daa282386@gmail.com>
2023-07-26 14:03:11 +0300

    0x01-caching_lfu

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""LFU caching module"""
+
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """LFU cache implementation."""
+
+    pass
